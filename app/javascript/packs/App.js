4d382ae5dd8b663ab22bbdb048f9bd8adc83fc5a
sameer <sameer.manandhar@itonics.de>
2020-02-15 13:22:53 +0545

    Boggle : Created App as man app file

---
+++ app/javascript/packs/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    <p> IT works</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default App;
